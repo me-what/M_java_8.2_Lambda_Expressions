7732a45f7f1e16f5fcd10a0c7738818a6bc5201d
withoutLastNameAndFirstName <eldar.979751@gmail.com>
2023-11-16 11:54:12 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        Expression func = (n)-> n%2==0;
+        int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        System.out.println(sum(nums, func));
+    }
+
+    private static int sum (int[] numbers, Expression func)
+    {
+        int result = 0;
+        for(int i : numbers)
+        {
+            if (func.isEqual(i)) {
+                result += i;
+            }
+        }
+        return result;
+    }
+}
